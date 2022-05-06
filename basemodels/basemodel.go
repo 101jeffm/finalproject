dc8425fe89ec1f5d0b1abdc3efa118ccdb0e90b7
Jeffrey Michaelis <47127711+101jeffm@users.noreply.github.com>
2022-05-06 08:20:56 -0600

    Add files via upload

--- basemodels/basemodel.go
+++ basemodels/basemodel.go
@@ -1,1 +1,16 @@
+package basemodels
 
+//CLI arguments to determine what payload and where to insert
+type CmdLineArgs struct {
+	Input    string
+	Output   string
+	Meta     bool
+	Suppress bool
+	Offset   string
+	Inject   bool
+	Payload  string
+	Type     string
+	Encode   bool
+	Decode   bool
+	Key      string
+}
